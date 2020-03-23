873fbc96273af0a94db4024f70100e12866675d6
leer <ladesigns90@gmail.com>
2020-03-23 02:18:26 -0400

    Started tyring t figure out how to add key to keep count of how many times got wrong or right.

--- lib/script.py
+++ lib/script.py
@@ -11,6 +11,12 @@ welcome = input('Welcome to the game Guesstimation! PRESS ENTER key to start:')
 # Random Shuffle content in states dictionaries
 random.shuffle(states)
 
+# Add Keys in dictionaries to store number of times right or wrong
+#count = 0
+#for count in range(len(states)):
+    #states["ricount"]= 0
+    #states["wrcount"]= 0
+
 #Loop thru states array of dictionaries to be able to print out indexed state
 for index in range(len(states)):
     for key in states[index]:
