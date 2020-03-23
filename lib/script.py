6da4f4fddef06682a3c4865f61a9ec7bb71237da
leer <ladesigns90@gmail.com>
2020-03-23 02:04:15 -0400

    Figured out how to Loop thru states array of dictionaries to be able to print out indexed state. Also added the method to Random Shuffle content in states dictionaries.

--- lib/script.py
+++ lib/script.py
@@ -1,3 +1,24 @@
 from capitals import states
+import random
 
-print(states)
+#Test to access dictionary
+print(states[3]['name'])
+#print(states)
+
+#Welcome Message:
+welcome = input('Welcome to the game Guesstimation! PRESS ENTER key to start:')
+
+# Random Shuffle content in states dictionaries
+random.shuffle(states)
+
+#Loop thru states array of dictionaries to be able to print out indexed state
+for index in range(len(states)):
+    for key in states[index]:
+        capital_guess = input('Where is capital of ' + states[index][key] + ':')
+
+
+
+
+
+
+
